67797fc9f3fb3d5a3666422666e12697f6f9071d
Danila Katorov <76109766+SfdJucide@users.noreply.github.com>
2023-02-22 14:21:34 +0300

    Add files via upload

---
+++ form/app.js
@@ -0,0 +1,35 @@
+let tg = window.Telegram.WebApp;
+
+tg.expand();
+
+tg.MainButton.textColor = "#FFFFFF";
+tg.MainButton.color = "#2cab37";
+
+let item = "";
+
+let btn1 = document.getElementById("btn1");
+
+// btn1.addEventListener("click", function(){
+// 	if (tg.MainButton.isVisible) {
+// 		tg.MainButton.hide();
+// 	}
+// 	else {
+// 		tg.MainButton.setText("Вы выбрали товар 1!");
+// 		item = "1";
+// 		tg.MainButton.show();
+// 	}
+// });
+
+Telegram.WebApp.onEvent("mainButtonClicked", function(){
+	tg.sendData(item);
+});
+
+let product_name = document.getElementById("product_name1");
+
+// let p = document.createElement("p");
+
+// p.innerText = `${tg.initDataUnsafe.user.first_name}
+// ${tg.initDataUnsafe.user.last_name}`;
+
+
+// usercard.appendChild(p); 
