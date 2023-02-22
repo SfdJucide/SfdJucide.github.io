3497a4447f55d211cab63de1cc834a4333c4b7c0
Danila Katorov <76109766+SfdJucide@users.noreply.github.com>
2023-02-22 14:49:40 +0300

    Add files via upload

--- form/app.js
+++ form/app.js
@@ -9,23 +9,9 @@ let item = "";
 
 let btn1 = document.getElementById("btn1");
 
-// btn1.addEventListener("click", function(){
-// 	if (tg.MainButton.isVisible) {
-// 		tg.MainButton.hide();
-// 	}
-// 	else {
-// 		tg.MainButton.setText("Вы выбрали товар 1!");
-// 		item = "1";
-// 		tg.MainButton.show();
-// 	}
-// });
-
 Telegram.WebApp.onEvent("mainButtonClicked", function(){
 	tg.sendData(item);
 });
-
-let product_name = document.getElementById("product_name1");
-
 // let p = document.createElement("p");
 
 // p.innerText = `${tg.initDataUnsafe.user.first_name}
