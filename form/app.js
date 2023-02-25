762a4c334c672058f24e80d30fa300ccdca0271f
Danila Katorov <76109766+SfdJucide@users.noreply.github.com>
2023-02-25 14:53:57 +0300

    Add files via upload

--- form/app.js
+++ form/app.js
@@ -5,17 +5,21 @@ tg.expand();
 tg.MainButton.textColor = "#FFFFFF";
 tg.MainButton.color = "#2cab37";
 
-let item = "";
+let form = document.getElementById('question_form');
+form.addEventListener('submit', getFormValue);
 
 let btn1 = document.getElementById("btn1");
 
 Telegram.WebApp.onEvent("mainButtonClicked", function(){
 	tg.sendData(item);
 });
-// let p = document.createElement("p");
 
-// p.innerText = `${tg.initDataUnsafe.user.first_name}
-// ${tg.initDataUnsafe.user.last_name}`;
+btn1.addEventListener('click', 	function() {
+	tg.sendData("some shit")
+});
 
-
-// usercard.appendChild(p); 
+function getFormValue(event) {
+    event.preventDefault();
+	let question = form.querySelector('[question="question"]')
+	console.log(question)
+}
